5211510dcfc3a691e17184f885f75a66911cf6f9
teerayutnz <pay18wos@gmail.com>
2021-07-08 11:15:45 +0700

    editing Game Logic

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
-
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
+}
 export default function WordCard(props) {
     const activationHandler = c => { console.log(`${c} has been activated.`) }
     return (
         <div>
-            {Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/>)}
+            {Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} />)}
         </div>
     );
 }
