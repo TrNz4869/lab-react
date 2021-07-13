9a41bcb6757c5776bbe5289e97b787f6090d5997
teerayutnz <pay18wos@gmail.com>
2021-07-13 16:32:28 +0700

    add input text to create card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import WordCard from './WordCard';
-const word = "Hello";
 function App() {
+  const [data,setData]=useState(null)
+  const [print,setPrint]=useState(false)
+  
+  function getData(val)
+  {
+    console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
+  }
   return (
-    <div>
-      <WordCard value={word}/>
+    <div className="App">
+     {
+       print?
+       <WordCard value={data}/>
+       :null
+     }
+    <input type="text" onChange={getData} />
+    <button onClick={()=>setPrint(true)} >Print Data</button>
     </div>
   );
 }
+
 
 export default App;
 
