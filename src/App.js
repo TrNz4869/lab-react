a816d87cc94ec803a0084f11de2062df147eced2
teerayutnz <pay18wos@gmail.com>
2021-07-14 08:11:58 +0700

    edit some ui

--- src/App.js
+++ src/App.js
@@ -17,23 +17,22 @@ function App() {
 
   return (
     <div className="App">
-    <div className="frame">
-    <h3>Welcome to Word Card Game</h3>
-    <b>Instruction</b><br></br>
-    <p>1. Type your word on typebox and press Enter button</p>
-    <p>2. Choose cards to make the correct word</p>
-    <input type="text" placeholder="Type your word here!" onChange={getData}/>&nbsp;&nbsp;
-    <button className="button" onClick={()=>setPrint(true)}>Enter</button>&nbsp;&nbsp;
-    </div>  
-    <div className="frame-2">
-    {
-       print?
-       <WordCard value={data}/>
-       :null 
-    }
-    </div>
-    <button className="button-2" onClick={restartGame}>Restart</button>
-
+      <div className="frame">
+        <h3>Welcome to Word Card Game</h3>
+        <b>Instruction</b><br></br>
+        <p>1. Type your word on typebox and press Enter button</p>
+        <p>2. Choose cards to make the correct word</p>
+        <input type="text" placeholder="Type your word here!" onChange={getData}/>&nbsp;&nbsp;
+        <button className="button" onClick={()=>setPrint(true)}>Enter</button>&nbsp;&nbsp;
+      </div>  
+      <div className="frame-2">
+        {
+          print?
+          <WordCard value={data}/>
+          :null 
+        }
+      </div>
+      <button className="button-2" onClick={restartGame}>Restart</button>
     </div>
   );
 }
